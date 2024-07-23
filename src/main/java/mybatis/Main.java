c9657a959383924eaefa42d3be7857d479312d5b
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-07-23 11:45:31 +0900

    :hammer: mybatis setting

--- src/main/java/mybatis/Main.java
+++ src/main/java/mybatis/Main.java
@@ -1,7 +1,9 @@
 package mybatis;
 
+// config : xml
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Mybatis 설정 파일을 읽어 온다.
+        // SQL
     }
 }
